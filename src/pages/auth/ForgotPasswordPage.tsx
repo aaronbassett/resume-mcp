81e6c42e9c8374209c79d6bde67a20fea4c8aff9
Aaron Bassett <aaronbassett@gmail.com>
2025-06-26 05:13:52 +0100

    Updated ForgotPasswordPage.tsx

--- src/pages/auth/ForgotPasswordPage.tsx
+++ src/pages/auth/ForgotPasswordPage.tsx
@@ -185,7 +185,7 @@ export const ForgotPasswordPage: FC = () => {
             <div className="space-y-4">
               <h2 className="text-5xl font-bold leading-tight">
                 Reset Your
-                <span class="block bg-clip-text text-transparent bg-gradient-to-r gradient-primary">
+                <span class="block bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
                   Password
                 </span>
               </h2>
