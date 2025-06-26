b6c920ec7b2e28f718ebc8b9a18098748f4211ab
Aaron Bassett <aaronbassett@gmail.com>
2025-06-26 05:47:58 +0100

    Updated flowbite-theme.ts

--- src/flowbite-theme.ts
+++ src/flowbite-theme.ts
@@ -27,8 +27,9 @@ export const customTheme: CustomFlowbiteTheme = {
           failure: "border-red-500 bg-red-50 text-red-900 placeholder-red-700 focus:border-red-500 focus:ring-red-500 dark:border-red-400 dark:bg-red-100 dark:focus:border-red-500 dark:focus:ring-red-500",
           warning: "border-yellow-500 bg-yellow-50 text-yellow-900 placeholder-yellow-700 focus:border-yellow-500 focus:ring-yellow-500 dark:border-yellow-400 dark:bg-yellow-100 dark:focus:border-yellow-500 dark:focus:ring-yellow-500",
           success: "border-green-500 bg-green-50 text-green-900 placeholder-green-700 focus:border-green-500 focus:ring-green-500 dark:border-green-400 dark:bg-green-100 dark:focus:border-green-500 dark:focus:ring-green-500",
-          // Custom auth color for authentication pages
-          auth: "bg-transparent border-0 border-b-2 border-white/20 text-white placeholder-white/60 focus:outline-none focus:ring-0"
+          // Custom input elements
+          auth: "bg-transparent border-0 border-b-2 border-white/20 text-white placeholder-white/60 focus:outline-none focus:ring-0",
+          search: "w-ful bg-primary/70 border-0 border-b-2 border-white/20 text-white placeholder-white/60 focus:outline-none focus:ring-0"
         },
         withRightIcon: {
           on: "pr-10",
