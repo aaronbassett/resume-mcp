7f97091bb91e47149991fc0c88d9cc52f74a1157
Aaron Bassett <aaronbassett@gmail.com>
2025-06-27 10:54:48 +0100

    Updated Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -66,7 +66,7 @@ export const Header: FC = () => {
                 duration={750}
                 toggled={theme === 'dark'}
                 onToggle={toggleTheme}
-                className="text-foreground hover:text-primary transition-colors text-base"
+                className="text-foreground hover:text-primary transition-colors text-lg"
               />
             </div>
 
